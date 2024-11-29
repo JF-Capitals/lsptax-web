97e82c66eff92a4ae06d46eaf5e445a512b2b478
mukulbbhardwaj <admin@yoursupportagent.com>
2024-11-30 01:03:23 +0530

    animation fix

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import "aos/dist/aos.css";
 // Initialize AOS
 AOS.init({
   offset: 200, // Distance from top
-  duration: 1000, // Animation duration
+  duration: 800, // Animation duration
   easing: "ease-in-out", // Easing
   once: false, // Whether animation should only run once
 });
